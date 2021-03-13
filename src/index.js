d080e71716338a9b19cefee0d2f182eda02764dd
kotser1 <newelvin@gmail.com>
2021-03-13 03:17:03 +0300

    feat: add convert function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+    const onesArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+                'seventeen', 'eighteen', 'nineteen'];
+    const tensArray = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+                'ninety'];
+    
+    const numString = number.toString();
+    
+    if (number < 0) {
+        throw new Error('Ошибка - введено отрицательное число. Число должно быть больше или равно 0');
+    }
+
+    if (number === 0) {
+        return 'zero';
+    } 
+    
+    if (number < 20) {
+        return onesArray[number];
+    }
+    
+    if (numString.length === 2) { 
+        return (tensArray[numString[0]] + ' ' + onesArray[numString[1]]).trim();
+    }
+    
+    if (numString.length == 3) {
+        if (numString[1] === '0' && numString[2] === '0') {
+            return onesArray[numString[0]] + ' hundred';
+        } else {
+            return onesArray[numString[0]] + ' hundred ' + toReadable(+(numString[1] + numString[2]));
+        }
+    }
 }
